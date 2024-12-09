114b1e55f3b48eae116491a765a635c25637e84b
Mahdeen Abrar <mahdeen02@gmail.com>
2024-12-09 01:49:19 -0400

    context driver has selection map now

--- Editor/Scripts/Context/IndieBuff_ContextDriver.cs
+++ Editor/Scripts/Context/IndieBuff_ContextDriver.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace IndieBuff.Editor
 {
     internal class IndieBuff_ContextDriver
     {
-        Dictionary<string, object> contextData;
         private static IndieBuff_ContextDriver _instance;
+        internal string ContextObjectString = "";
+
 
         internal static IndieBuff_ContextDriver Instance
         {
@@ -22,6 +24,7 @@ namespace IndieBuff.Editor
         public void BuildAllContext(string prompt)
         {
             // build user selected context
+            Dictionary<string, object> selectionMap = IndieBuff_UserSelectedContext.Instance.BuildUserContext();
 
 
             // build code context
@@ -29,6 +32,23 @@ namespace IndieBuff.Editor
             // build scene context
 
             // build asset context
+            Dictionary<string, object> assetMap = new Dictionary<string, object>();
+
+            var settings = new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                Formatting = Formatting.Indented,
+                MaxDepth = null,
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            var contextData = new
+            {
+                selectionMap,
+                assetMap,
+            };
+
+            ContextObjectString = JsonConvert.SerializeObject(new { context = contextData }, settings);
 
         }
 
