1d0b5549e5cfe1944fa69aafac059f01557a4a4a
Mahdeen Abrar <mahdeen02@gmail.com>
2024-12-29 21:29:01 -0400

    added command links

--- Editor/Scripts/Models/IndieBuff_ChatModeData.cs
+++ Editor/Scripts/Models/IndieBuff_ChatModeData.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace IndieBuff.Editor
 {
@@ -10,6 +11,36 @@ namespace IndieBuff.Editor
         Prototype,
         Debug,
         Onboard,
+    }
+
+    public static class IndieBuff_ChatModeCommands
+    {
+        public static readonly Dictionary<string, ChatMode> CommandMappings = new Dictionary<string, ChatMode>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "/chat", ChatMode.Chat },
+            { "/cmd", ChatMode.Command },
+            { "/prototype", ChatMode.Prototype },
+            { "/debug", ChatMode.Debug },
+            { "/onboard", ChatMode.Onboard }
+        };
+
+        public static readonly Dictionary<string, string> CommandAliases = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "/c", "/chat" },
+            { "/p", "/prototype" },
+            { "/d", "/debug" },
+            { "/o", "/onboard" }
+        };
+
+        public static bool TryGetChatMode(string command, out ChatMode mode)
+        {
+            if (CommandAliases.TryGetValue(command, out string mainCommand))
+            {
+                command = mainCommand;
+            }
+
+            return CommandMappings.TryGetValue(command, out mode);
+        }
     }
 
 }
